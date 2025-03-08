a88c936a4aae06d8ad9a82d21849b0bd377f97fd
augustoEstudante <augusto.souza@estudante.ifto.edu.br>
2025-03-07 21:44:35 -0300

    Aula07Março

--- Aula06Março/css externo/Script.js
+++ Aula06Março/css externo/Script.js
@@ -1,4 +1,12 @@
 function amarelo(){
-    let button = document.getElementById ("item2");
+    let button = document.getElementById ("Item2");
     button.style.backgroundColor = "red";
+}
+
+
+function white() {
+    let texto2 = document.getElementById("item1")
+    texto2.style.backgroundColor = "white";
+
+    
 }
